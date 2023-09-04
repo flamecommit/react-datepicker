a41ccfa89b9076ddc2ee5cbe8a342315833bcee7
신용준 <shinyj1991@wemadeazoutlook.onmicrosoft.com>
2023-09-04 13:13:14 +0900

    feat : eslint

--- test/src/index.js
+++ test/src/index.js
@@ -1,10 +1,10 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import App from "./App";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
